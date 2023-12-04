9879101fc729419e0619297824e2be1da9bc4576
RodrigoTorrico <144338955+RodrigoTorrico@users.noreply.github.com>
2023-12-04 06:43:59 +0100

    Add files via upload

---
+++ source/Orbits.py
@@ -0,0 +1,13 @@
+
+from numpy import array
+
+def Kepler(U,t):
+    
+    x , y, vx, vy = U
+    m = (x ** 2 + y ** 2) ** 0.5
+    
+    if m == 0:
+        # Evita la división por cero
+        return array([vx, vy, 0, 0])
+    
+    return array ([vx, vy, -x/m, -y/m])
