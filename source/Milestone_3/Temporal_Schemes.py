aa2b907e25d404858d3f1118ddad5110d8872860
RodrigoTorrico <144338955+RodrigoTorrico@users.noreply.github.com>
2023-12-15 23:19:01 +0100

    Update Temporal_Schemes.py

--- source/Milestone_3/Temporal_Schemes.py
+++ source/Milestone_3/Temporal_Schemes.py
@@ -1,17 +1,17 @@
-
 from scipy.optimize import newton
 
 def Euler(U, t1, t2, F):
     
-    return U + (t2 - t1) * F(U, t1)
+    return U + (t2 - t1) * F(U, t2)
 
 def Crank_Nicolson(U, t1, t2, F):
     
     def ResidualCN(X):
         
-        return X - U - (t2 -t1)/2 * (F(U, t1) + F(X, t2))
+        return X - U - (t2 -t1)/2 * (F(U, t1) + F(U, t2)) - (t2 -t1)/2 * (F(X, t1) + F(X, t2 + (t2 - t1)))
     
     return newton(ResidualCN, U)
+
 
 def Inverse_Euler(U, t1, t2, F):
 
@@ -29,4 +29,3 @@ def RK4(U, t1, t2, F):
     k4 = F(U + (t2 - t1) * k3, t2 + (t2 - t1))
     
     return U + (t2 - t1) * (k1 + 2*k2 + 2*k3 + k4)/6
-    
